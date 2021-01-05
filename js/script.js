0f2d1b62c723b1fd4fd221d56958a756af5b5827
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-01-05 15:55:09 +0800

    map hover

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ for(let i=1; i<=5; i++){
 }
 
 /* map */ 
+
 $(document).ready(function(){
 	for(let i=1; i<=13; i++)
 	$('.district_'+i).click(function(){
@@ -31,4 +32,20 @@ $(document).ready(function(){
 	$('.district_c').click(function(){
 		$('.box_map_explain').toggleClass('menu-click')
 	});
+	/* map hover - maphilight */ 
+	$.fn.maphilight.defaults = {
+		fill: true,
+		fillColor: '000000',
+		fillOpacity: 0.2,
+		stroke: true,
+		strokeColor: 'ff0000',
+		strokeOpacity: 0,
+		strokeWidth: 1,
+		fade: true,
+		alwaysOn: false
+	}
+	$(function() {
+		$('.map').maphilight();
+	});
 });
+
