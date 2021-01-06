c704b9c025a1ffac89f70a2631b8cd97cb62ba1d
cianyastone <lincofe@gmail.com>
2021-01-07 00:08:36 +0800

    yaa

--- js/script.js
+++ js/script.js
@@ -22,13 +22,4 @@ for(let i=1; i<=5; i++){
 	});
 }
 
-$(".paroller, [data-paroller-factor]").paroller({
-	factor: 0.1,            // multiplier for scrolling speed and offset
-	factorXs: 0.1,          // multiplier for scrolling speed and offset if window width is <576px
-	factorSm: 0.2,          // multiplier for scrolling speed and offset if window width is <=768px
-	factorMd: 0.2,          // multiplier for scrolling speed and offset if window width is <=1024px
-	factorLg: 0.3,          // multiplier for scrolling speed and offset if window width is <=1200px
-	type: 'foreground',     // background, foreground
-	direction: 'vertical', // vertical, horizontal
-	transition: 'transform 0.2s ease-in'
-});
+
